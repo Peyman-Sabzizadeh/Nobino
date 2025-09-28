d5fde8b59c5f3ff5c05647ae212b3b3ed08a4b60
Peyman-Sabzizade <peyman.front.developer@gmail.com>
2025-09-28 11:29:52 +0330

    feat(ui): add HeroUI component library

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Providers } from "./_providers/providers";
 
 export const metadata: Metadata = {
   title: "نوبینو: دستیار هوشمند شما برای رزرو نوبت",
@@ -14,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa" dir="rtl">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
