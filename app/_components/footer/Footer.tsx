32808ef1b3e6c112d35857498697059c32bb959d
Peyman-Sabzizade <peyman.front.developer@gmail.com>
2025-10-30 19:19:53 +0330

    feat(ui): add Footer section

---
+++ app/_components/footer/Footer.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Divider } from "@heroui/react";
+import { HeartHandshake } from "lucide-react";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <div className="mt-2 flex flex-col justify-between gap-6 rounded-sm bg-blue-500 p-4 text-amber-50 dark:bg-indigo-500">
+      <p className="flex gap-2">
+        <HeartHandshake />
+        هر قدمی که برمی‌داریم با هدف ساختن دنیایی مطمئن‌تر برای شماست. نوبینو
+        یعنی اعتماد، آرامش و همراهی.
+      </p>
+      <Divider />
+      <div className="flex items-center justify-around *:flex *:flex-col">
+        <div className="space-y-3">
+          <p className="mb-4 text-xl font-bold">دسترسی سریع</p>
+          <Link href="/">خانه</Link>
+          <Link href="/">رزرو نوبت</Link>
+          <Link href="/">نوبت های من</Link>
+          <Link href="/">درباره ما</Link>
+        </div>
+        <div className="space-y-3">
+          <p className="mb-4 text-xl font-bold">خدمات مشتریان</p>
+          <Link href="/">پرسش های متداول</Link>
+          <Link href="/">پیگیری نوبت</Link>
+          <Link href="/">راهنمای رزرو نوبت</Link>
+          <Link href="/">تماس با ما</Link>
+        </div>
+        <div className="space-y-3">
+          <p className="mb-4 text-xl font-bold">قوانین و مقررات</p>
+          <Link href="/">قوانین استفاده</Link>
+          <Link href="/">حریم خصوصی</Link>
+          <Link href="/">شکایات</Link>
+          <Link href="/">پشتیبانی</Link>
+        </div>
+      </div>
+      <Divider />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
 import Header from "@/app/_components/header/Header";
-import Hero from "@/app/_components/header/components/Hero";
+import Hero from "@/app/_components/hero/Hero";
+import Footer from "@/app/_components/footer/Footer";
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-4">
-      <Header />
-      <Hero />
-    </div>
+    <>
+      <div className="container mx-auto px-4">
+        <Header />
+        <Hero />
+      </div>
+      <Footer />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </>
   );
 }
