277e5fc47cf86f3c40e4a6b4f21a5e9ddc18b59b
Peyman-Sabzizade <peyman.front.developer@gmail.com>
2025-11-28 16:21:45 +0330

    style(filters): edit the filter section

--- app/_components/Appointment/index.tsx
+++ app/_components/Appointment/index.tsx
@@ -1,5 +1,7 @@
 import { getFilterItems } from "@/app/_lib/actions/filter";
 import Filter from "./components/Filter";
+import { Divider } from "@heroui/react";
+import { FilterIcon } from "lucide-react";
 
 export default async function Appointment() {
   const {
@@ -11,7 +13,12 @@ export default async function Appointment() {
 
   return (
     <div className="mt-2 grid grid-cols-[1fr_4fr] gap-3 max-md:grid-cols-1">
-      <div className="rounded-lg bg-gray-200 p-4 dark:bg-gray-600">
+      <div className="space-y-2 rounded-lg bg-gray-200 p-4 dark:bg-gray-600">
+        <h3 className="text-center flex items-center gap-1 justify-center">
+          <FilterIcon size="18px" />
+          فیلتر
+        </h3>
+        <Divider />
         <Filter title="استان" items={uniqueProvinces} />
         <Filter title="شهر" items={uniqueCities} />
         <Filter title="تخصص" items={uniqueSpecialty} />
