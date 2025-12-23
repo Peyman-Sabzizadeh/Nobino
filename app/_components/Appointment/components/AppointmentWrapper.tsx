e44dcbeeed76ada47df1fa6005e8b9aea1659656
Peyman-Sabzizade <peyman.front.developer@gmail.com>
2025-12-23 23:28:16 +0330

    feat(url): show the province item from url

--- app/_components/Appointment/components/AppointmentWrapper.tsx
+++ app/_components/Appointment/components/AppointmentWrapper.tsx
@@ -1,15 +1,26 @@
 "use client";
 
+import { useEffect } from "react";
 import FilterSection from "./FilterSection";
 import MobileFilter from "./MobileFilter";
 import useIsMobile from "@/app/_hooks/useIsMobile";
 import type { FilterDataType } from "@/app/_types/filter";
+import { useSearchParams } from "next/navigation";
+import { useFilterStore } from "@/app/_store/filterStore";
 
 export default function AppointmentWrapper({
   filterData,
 }: {
   filterData: FilterDataType;
 }) {
+  const searchParams = useSearchParams();
+  const setProvince = useFilterStore((state) => state.setProvince);
+  useEffect(() => {
+    const province = searchParams.get("province");
+    if (province) {
+      setProvince(new Set([province]));
+    }
+  });
   const isMobile = useIsMobile();
   return (
     <div className="mt-2 grid grid-cols-[1fr_4fr] gap-3 max-md:grid-cols-1">
