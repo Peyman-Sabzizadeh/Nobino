caaa6d940876e6d6f0e2463ab3f2c29d1f4781b7
Peyman-Sabzizade <peyman.front.developer@gmail.com>
2026-01-07 16:48:25 +0330

    feat(doctor): add ui structure of DoctorsList component

--- app/_components/Appointment/components/doctors/DoctorsList.tsx
+++ app/_components/Appointment/components/doctors/DoctorsList.tsx
@@ -1,9 +1,15 @@
+import { Stethoscope } from "lucide-react";
 import Doctor from "./Doctor";
+import { Divider } from "@heroui/react";
 
 export default function DoctorsList() {
   return (
-    <div>
-      <h1>Doctors list shown here 👇🏼</h1>
+    <div className="space-y-2 rounded-lg bg-gray-200 p-4 dark:bg-gray-600">
+      <h3 className="flex items-center gap-1 text-center">
+        <Stethoscope size="18px" />
+        دکترها
+      </h3>
+      <Divider />
       <Doctor />
       <Doctor />
       <Doctor />
