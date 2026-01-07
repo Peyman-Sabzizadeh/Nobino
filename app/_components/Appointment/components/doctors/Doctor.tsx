49a65f8cc385630f60c4a7c9b163048032ecd41b
Peyman-Sabzizade <peyman.front.developer@gmail.com>
2026-01-07 23:16:53 +0330

    faet(doctor): add ui structure of Doctor component

--- app/_components/Appointment/components/doctors/Doctor.tsx
+++ app/_components/Appointment/components/doctors/Doctor.tsx
@@ -1,3 +1,10 @@
 export default function Doctor() {
-  return <p>Doctor component</p>
+  return (
+    <div className="rounded-lg bg-blue-400 px-4 py-2">
+      {/* The below informations maybe change later.. */}
+      <div>Pic, Medical system code, Experience</div>
+      <div>Name & Family, Specialty, Phone, Address</div>
+      <div>City, Reserve button</div>
+    </div>
+  );
 }
