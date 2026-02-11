734e53b460aafdc639e8ac1593df040484749aed
Peyman-Sabzizade <peyman.front.developer@gmail.com>
2026-02-11 07:59:17 +0330

    feat(doctor): add some basic styles to the content

--- app/_components/Appointment/components/doctors/Doctor.tsx
+++ app/_components/Appointment/components/doctors/Doctor.tsx
@@ -1,10 +1,21 @@
 export default function Doctor() {
   return (
-    <div className="rounded-lg bg-blue-400 px-4 py-2">
-      {/* The below informations maybe change later.. */}
-      <div>Pic, Medical system code, Experience</div>
-      <div>Name & Family, Specialty, Phone, Address</div>
-      <div>City, Reserve button</div>
+    <div className="flex items-center justify-between rounded-lg bg-blue-400 px-4 py-2 *:flex *:flex-col">
+      <div>
+        <h3>Pic</h3>
+        <h3>Medical system code</h3>
+        <h3>Experience</h3>
+      </div>
+      <div>
+        <h3>Name & Family</h3>
+        <h3>Specialty</h3>
+        <h3>Phone</h3>
+        <h3>Address</h3>
+      </div>
+      <div>
+        <h3>City</h3>
+        <h3>Reserve button</h3>
+      </div>
     </div>
   );
 }
